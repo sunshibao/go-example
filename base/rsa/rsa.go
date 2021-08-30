a4504498de642071c1d3986293fbf4cfe3e2bcb0
sunshibao <664588619@qq.com>
2021-08-30 16:07:37 +0800

    aes、rsa加解密

--- base/rsa/rsa.go
+++ base/rsa/rsa.go
@@ -4,45 +4,12 @@ import (
 	"crypto/rand"
 	"crypto/rsa"
 	"crypto/x509"
-	"encoding/base64"
 	"encoding/pem"
 	"errors"
-	"fmt"
 )
 
-// 可通过openssl产生
-//openssl genrsa -out rsa_private_key.pem 1024
-var privateKey = []byte(`
------BEGIN RSA PRIVATE KEY-----
-MIICXQIBAAKBgQDfw1/P15GQzGGYvNwVmXIGGxea8Pb2wJcF7ZW7tmFdLSjOItn9
-kvUsbQgS5yxx+f2sAv1ocxbPTsFdRc6yUTJdeQolDOkEzNP0B8XKm+Lxy4giwwR5
-LJQTANkqe4w/d9u129bRhTu/SUzSUIr65zZ/s6TUGQD6QzKY1Y8xS+FoQQIDAQAB
-AoGAbSNg7wHomORm0dWDzvEpwTqjl8nh2tZyksyf1I+PC6BEH8613k04UfPYFUg1
-0F2rUaOfr7s6q+BwxaqPtz+NPUotMjeVrEmmYM4rrYkrnd0lRiAxmkQUBlLrCBiF
-u+bluDkHXF7+TUfJm4AZAvbtR2wO5DUAOZ244FfJueYyZHECQQD+V5/WrgKkBlYy
-XhioQBXff7TLCrmMlUziJcQ295kIn8n1GaKzunJkhreoMbiRe0hpIIgPYb9E57tT
-/mP/MoYtAkEA4Ti6XiOXgxzV5gcB+fhJyb8PJCVkgP2wg0OQp2DKPp+5xsmRuUXv
-720oExv92jv6X65x631VGjDmfJNb99wq5QJBAMSHUKrBqqizfMdOjh7z5fLc6wY5
-M0a91rqoFAWlLErNrXAGbwIRf3LN5fvA76z6ZelViczY6sKDjOxKFVqL38ECQG0S
-pxdOT2M9BM45GJjxyPJ+qBuOTGU391Mq1pRpCKlZe4QtPHioyTGAAMd4Z/FX2MKb
-3in48c0UX5t3VjPsmY0CQQCc1jmEoB83JmTHYByvDpc8kzsD8+GmiPVrausrjj4p
-y2DQpGmUic2zqCxl6qXMpBGtFEhrUbKhOiVOJbRNGvWW
------END RSA PRIVATE KEY-----
-`)
-
-//openssl
-//openssl rsa -in rsa_private_key.pem -pubout -out rsa_public_key.pem
-var publicKey = []byte(`
------BEGIN PUBLIC KEY-----
-MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDfw1/P15GQzGGYvNwVmXIGGxea
-8Pb2wJcF7ZW7tmFdLSjOItn9kvUsbQgS5yxx+f2sAv1ocxbPTsFdRc6yUTJdeQol
-DOkEzNP0B8XKm+Lxy4giwwR5LJQTANkqe4w/d9u129bRhTu/SUzSUIr65zZ/s6TU
-GQD6QzKY1Y8xS+FoQQIDAQAB
------END PUBLIC KEY-----
-`)
-
 // 加密
-func RsaEncrypt(origData []byte) ([]byte, error) {
+func RsaEncrypt(origData, publicKey []byte) ([]byte, error) {
 	//解密pem格式的公钥
 	block, _ := pem.Decode(publicKey)
 	if block == nil {
@@ -60,7 +27,7 @@ func RsaEncrypt(origData []byte) ([]byte, error) {
 }
 
 // 解密
-func RsaDecrypt(ciphertext []byte) ([]byte, error) {
+func RsaDecrypt(ciphertext, privateKey []byte) ([]byte, error) {
 	//解密
 	block, _ := pem.Decode(privateKey)
 	if block == nil {
@@ -73,10 +40,4 @@ func RsaDecrypt(ciphertext []byte) ([]byte, error) {
 	}
 	// 解密
 	return rsa.DecryptPKCS1v15(rand.Reader, priv, ciphertext)
-}
-func main() {
-	data, _ := RsaEncrypt([]byte("http://c.biancheng.net/golang/"))
-	fmt.Println(base64.StdEncoding.EncodeToString(data))
-	origData, _ := RsaDecrypt(data)
-	fmt.Println(string(origData))
 }
---
+++ base/rsa/rsa_test.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"encoding/base64"
+	"fmt"
+	"testing"
+)
+
+func TestRsa(t *testing.T) {
+	// 可通过openssl产生
+	//openssl genrsa -out rsa_private_key.pem 1024
+	var privateKey = []byte(`
+-----BEGIN RSA PRIVATE KEY-----
+MIICXQIBAAKBgQDfw1/P15GQzGGYvNwVmXIGGxea8Pb2wJcF7ZW7tmFdLSjOItn9
+kvUsbQgS5yxx+f2sAv1ocxbPTsFdRc6yUTJdeQolDOkEzNP0B8XKm+Lxy4giwwR5
+LJQTANkqe4w/d9u129bRhTu/SUzSUIr65zZ/s6TUGQD6QzKY1Y8xS+FoQQIDAQAB
+AoGAbSNg7wHomORm0dWDzvEpwTqjl8nh2tZyksyf1I+PC6BEH8613k04UfPYFUg1
+0F2rUaOfr7s6q+BwxaqPtz+NPUotMjeVrEmmYM4rrYkrnd0lRiAxmkQUBlLrCBiF
+u+bluDkHXF7+TUfJm4AZAvbtR2wO5DUAOZ244FfJueYyZHECQQD+V5/WrgKkBlYy
+XhioQBXff7TLCrmMlUziJcQ295kIn8n1GaKzunJkhreoMbiRe0hpIIgPYb9E57tT
+/mP/MoYtAkEA4Ti6XiOXgxzV5gcB+fhJyb8PJCVkgP2wg0OQp2DKPp+5xsmRuUXv
+720oExv92jv6X65x631VGjDmfJNb99wq5QJBAMSHUKrBqqizfMdOjh7z5fLc6wY5
+M0a91rqoFAWlLErNrXAGbwIRf3LN5fvA76z6ZelViczY6sKDjOxKFVqL38ECQG0S
+pxdOT2M9BM45GJjxyPJ+qBuOTGU391Mq1pRpCKlZe4QtPHioyTGAAMd4Z/FX2MKb
+3in48c0UX5t3VjPsmY0CQQCc1jmEoB83JmTHYByvDpc8kzsD8+GmiPVrausrjj4p
+y2DQpGmUic2zqCxl6qXMpBGtFEhrUbKhOiVOJbRNGvWW
+-----END RSA PRIVATE KEY-----
+`)
+
+	//openssl
+	//openssl rsa -in rsa_private_key.pem -pubout -out rsa_public_key.pem
+	var publicKey = []byte(`
+-----BEGIN PUBLIC KEY-----
+MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDfw1/P15GQzGGYvNwVmXIGGxea
+8Pb2wJcF7ZW7tmFdLSjOItn9kvUsbQgS5yxx+f2sAv1ocxbPTsFdRc6yUTJdeQol
+DOkEzNP0B8XKm+Lxy4giwwR5LJQTANkqe4w/d9u129bRhTu/SUzSUIr65zZ/s6TU
+GQD6QzKY1Y8xS+FoQQIDAQAB
+-----END PUBLIC KEY-----
+`)
+
+	orig := "http://c.biancheng.net/golang/"
+	fmt.Println("原文：", orig)
+	encryptCode, err := RsaEncrypt([]byte(orig), publicKey)
+	if err != nil {
+		fmt.Errorf(err.Error())
+	}
+
+	fmt.Println("密文：", base64.StdEncoding.EncodeToString(encryptCode)) //密文要用base64转换
+
+	decryptCode, err := RsaDecrypt(encryptCode, privateKey)
+	if err != nil {
+		fmt.Errorf(err.Error())
+	}
+	fmt.Println("解密结果：", string(decryptCode))
+}
